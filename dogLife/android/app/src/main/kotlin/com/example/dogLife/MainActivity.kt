4ef118ef4e05d3834afdb1cc78104ef0edcc1cd7
KailanySousa <kailany-sousa2010@hotmail.com>
2020-08-22 19:05:31 -0300

    layout login page

---
+++ dogLife/android/app/src/main/kotlin/com/example/dogLife/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dogLife
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ dogLife/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:dogLife/pages/loginPage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dog Life',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
